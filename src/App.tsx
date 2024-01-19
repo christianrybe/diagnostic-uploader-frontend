2a18e2cbfc427a4823869dba5478a170bf1acd56
rybarczykchris <rybarczyk.chris@gmail.com>
2024-01-19 17:39:22 +0100

    refactor: runs lint on tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { Box, Container, CssBaseline } from '@mui/material';
-import './App.css';
-import FileUpload from './FileUpload';
+import { Box, Container, CssBaseline } from "@mui/material";
+import "./App.css";
+import FileUpload from "./FileUpload";
 
 export default function App() {
-
-
   return (
-      <Container component="main" maxWidth="sm">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >        
-          <FileUpload title={'Upload your .tgz file'}  />
-        </Box>
-      </Container>
+    <Container component="main" maxWidth="sm">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <FileUpload title={"Upload your .tgz file"} />
+      </Box>
+    </Container>
   );
 }

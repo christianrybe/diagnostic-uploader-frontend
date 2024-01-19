ae72669464398c2e9cbd8bff4b19b71352db0090
rybarczykchris <rybarczyk.chris@gmail.com>
2024-01-19 17:10:54 +0100

    fix: uses Dialog instead of alert

--- src/FileUpload.tsx
+++ src/FileUpload.tsx
@@ -1,15 +1,19 @@
-import { Grid, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import React, { useState } from 'react';
+import { Grid, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
+import ResultDialog from "./ResultDialog";
 
 interface FileUploadProps {
-    title: string;
-  }
+  title: string;
+}
 
-const FileUpload = ({title} : FileUploadProps) => {
-    const [file, setFile] = useState<File | null>(null);
-  const [fileName, setFileName] = useState('');
+const FileUpload = ({ title }: FileUploadProps) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState("");
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogContent, setDialogContent] = useState("");
+  const [dialogTitle, setDialogTitle] = useState("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files ? event.target.files[0] : null;
@@ -19,7 +23,7 @@ const FileUpload = ({title} : FileUploadProps) => {
     }
   };
 
-  if (process.env.REACT_APP_BACKEND_URL === undefined || process.env.REACT_APP_ARCHIVE_URL === undefined)   {
+  if (process.env.REACT_APP_BACKEND_URL === undefined || process.env.REACT_APP_ARCHIVE_URL === undefined) {
     throw new Error("REACT_APP_BACKEND_URL or REACT_APP_ARCHIVE_URL is not defined");
   }
 
@@ -28,24 +32,33 @@ const FileUpload = ({title} : FileUploadProps) => {
   const handleUpload = async () => {
     if (file) {
       const formData = new FormData();
-      formData.append('file', file);
+      formData.append("file", file);
 
       try {
         const response = await fetch(archiveUrl, {
-          method: 'POST',
+          method: "POST",
           body: formData,
         });
 
         if (response.ok) {
-          alert('File uploaded successfully');
+          setDialogContent("File uploaded successfully");
+          setDialogOpen(true);
+          setDialogTitle("Done");
         } else {
-          alert('File upload failed');
+          const errorData = await response.json();
+          setDialogContent(errorData.error.message);
+          setDialogOpen(true);
+          setDialogTitle("Error!");
         }
       } catch (error) {
-        alert('Error while uploading file');
-        console.error('Upload error:', error);
+        alert("Error while uploading file");
+        console.error("Upload error:", error);
       }
     }
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
   };
 
   return (
@@ -53,30 +66,25 @@ const FileUpload = ({title} : FileUploadProps) => {
       <Grid item>
         <Typography variant="h5">{title}</Typography>
       </Grid>
-      <input
-        accept="*/*"
-        style={{ display: 'none' }}
-        id="contained-button-file"
-        multiple
-        type="file"
-        onChange={handleFileChange}
-      />
-      <Grid item sx={{ width: '80%' }}>
+      <input accept="*/*" style={{ display: "none" }} id="contained-button-file" multiple type="file" onChange={handleFileChange} />
+      <Grid item sx={{ width: "80%" }}>
         <TextField fullWidth label="Selected File" value={fileName} margin="normal" InputProps={{ readOnly: true }} disabled={true} />
       </Grid>
-      <Grid item>    
-      {!fileName && 
-        <label htmlFor="contained-button-file">
-          <Button variant="contained" component="span">
-            Choose File
+      <Grid item>
+        {!fileName && (
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" component="span">
+              Choose File
+            </Button>
+          </label>
+        )}
+        {fileName && (
+          <Button variant="contained" color="primary" onClick={handleUpload} disabled={!file}>
+            Upload File
           </Button>
-        </label>
-        }
-        {fileName && <Button variant="contained" color="primary" onClick={handleUpload} disabled={!file}>
-          Upload File
-        </Button>}
+        )}
       </Grid>
-      
+      <ResultDialog open={dialogOpen} handleClose={handleCloseDialog} title={dialogTitle} content={dialogContent} />
     </Grid>
   );
 };
---
+++ src/ResultDialog.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+
+const ResultDialog = ({ open, handleClose, title, content }: { open: boolean; handleClose: () => void; title: string; content: string }) => {
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ResultDialog;
